4d2b22f3be080d0a6db594dd7f6f1ec53d8ddd3f
sorakim <sora6418@gmail.com>
2024-06-18 11:29:01 +0900

    Feat: add Modal components

---
+++ src/components/Modal.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const Modal = () => {
+
+  return (
+    <Container>
+        Modal
+    </Container>
+  )
+}
+
+const Container = styled.div`
+  
+`;
+
+
+export default Modal
