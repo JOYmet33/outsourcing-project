925b23d12e2664b6ea295d1821a139fdbc68eeec
mangmuse <mangmangmuse@gmail.com>
2024-06-20 02:12:57 +0900

    Update: add transition on sidebar

--- src/components/SideBar/CampSiteList/CampSiteList.styled.jsx
+++ src/components/SideBar/CampSiteList/CampSiteList.styled.jsx
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
+  width: 400px;
   padding: 16px 8px;
+  background-color: black;
 `;
--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { SIDE_BAR_TABS } from "../../constants/sideBarTabConstants";
 import SideBarHome from "./SideBarHome/SideBarHome";
 import SideBarReviews from "./SideBarReviews/SideBarReviews";
@@ -19,25 +19,38 @@ const initialState = SIDE_BAR_TABS[0];
 const SideBar = ({ selectedSite }) => {
   const isSideBarOpened = useCampsiteStore((state) => state.isSideBarOpened);
   const [activeTab, setActiveTab] = useState(initialState);
+  const [shouldRender, setShouldRender] = useState(isSideBarOpened);
+
   const handleTapClick = (tap) => {
     setActiveTab(tap);
   };
 
+  useEffect(() => {
+    let timer;
+    if (isSideBarOpened) {
+      setShouldRender(true);
+    } else {
+      timer = setTimeout(() => setShouldRender(false), 250);
+    }
+    return () => clearTimeout(timer);
+  }, [isSideBarOpened]);
+
   const ActiveComponent = sideBarComponents[`SideBar${activeTab}`];
-  if (isSideBarOpened)
-    return (
-      <Wrapper>
-        {selectedSite && (
-          <CampSiteDetail
-            selectedSite={selectedSite}
-            activeTab={activeTab}
-            ActiveComponent={ActiveComponent}
-            onClick={handleTapClick}
-          />
-        )}
-        <CampSiteList />
-      </Wrapper>
-    );
+  if (!shouldRender) return null;
+
+  return (
+    <Wrapper $isSideBarOpened={isSideBarOpened}>
+      {selectedSite && (
+        <CampSiteDetail
+          selectedSite={selectedSite}
+          activeTab={activeTab}
+          ActiveComponent={ActiveComponent}
+          onClick={handleTapClick}
+        />
+      )}
+      <CampSiteList />
+    </Wrapper>
+  );
 };
 
 export default SideBar;
