a4c4799b0a5bd35c78071a47946ac204ffbc8649
riverSun1 <haeweon0426@gmail.com>
2024-06-20 03:20:10 +0900

    Update: Header에 현재 로그인 유저 프로필 이미지와 이름 표시

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,37 @@
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import supabase from "../../supabaseClient";
 import { IconMyPage } from "../Icon/Icons/IconMyPage";
-import { Button, ButtonContainer, HeaderBar, HeaderLink, IconContainer, LogoContainer } from "./Header.styled";
+import { Button, ButtonContainer, HeaderBar, HeaderLink, IconContainer, Img, LogoContainer } from "./Header.styled";
 import SearchContainer from "./SearchContainer/SearchContainer";
 
 const Header = () => {
+  const [userName, setUserName] = useState("");
+  const [userImage, setUserImage] = useState("");
+
+  async function checkLogin() {
+    const session = await supabase.auth.getSession();
+    const user = session.data.session?.user;
+
+    if (user) {
+      const { data, error } = await supabase.from("users").select("image, nickname").eq("id", user.id).single();
+
+      if (error) {
+        console.error("Error fetching user info:", error);
+      } else {
+        setUserImage(data.image);
+        setUserName(data.nickname);
+      }
+    }
+  }
+  useEffect(() => {
+    checkLogin();
+  }, []);
+
   const navigate = useNavigate();
 
   const handleLogin = () => {
-    navigate("/login");
+    navigate("/sign_in");
   };
 
   const moveToMyPage = () => {
@@ -22,7 +46,8 @@ const Header = () => {
       <SearchContainer />
       <ButtonContainer>
         <IconContainer onClick={moveToMyPage}>
-          <IconMyPage src={IconMyPage} />
+          {userImage ? <Img src={userImage} /> : <IconMyPage src={IconMyPage} />}
+          <div>{userName ? `${userName}님` : ""}</div>
         </IconContainer>
         <Button onClick={handleLogin}>로그인</Button>
       </ButtonContainer>
