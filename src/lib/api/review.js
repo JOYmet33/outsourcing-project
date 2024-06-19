66115d7ee6f359b501d2649dc095906191edbde9
sorakim <sora6418@gmail.com>
2024-06-19 22:10:22 +0900

    Feat: review crud api 추가

---
+++ src/lib/api/review.js
@@ -0,0 +1,26 @@
+import supabase from "../../supabaseClient.js";
+
+export const getReview = async () => {
+  const {data, error} = await supabase.from('review').select();
+  if(error) {
+    console.log('@@ error', error)
+  }
+  return data;
+}
+export const addReview = async (newReview) => {
+  const {data,error} = await supabase.from('review').insert(newReview).select()
+  if (error) {
+    console.log('@@ add ReviewError', error)
+  }
+  return data;
+}
+export const updateReview = async (editReview,id) => {
+  const {data, error} = await supabase.from('review').update(editReview).eq('id',id)
+  if(error) {
+    console.log('@@ error', error)
+  }
+  return data;
+}
+export const deleteReview = async (id) => {
+  await supabase.from('review').delete().eq('id',id)
+}
