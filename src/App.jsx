7e4a70c0016c36e44c73aee2f067914c6350cedc
Leeseayoung <tnfusrltk12@naver.com>
2024-06-20 23:37:12 +0900

    Update: 병합 충돌 해결

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import GlobalStyle from "../style/GlobalStyle";
 import HomeLayout from "./components/layouts/HomeLayout";
-import SignIn from "./pages/SignIn";
-import SignUp from "./pages/SignUp";
+import SignIn from "./pages/Sign/SignIn";
+import SignUp from "./pages/Sign/SignUp";
 import Mypage from "./pages/MyPage/MyPage";
-import Modal from "./components/Modal/Modal";
 
 const App = () => {
   return (
