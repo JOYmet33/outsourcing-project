9b1d20dc991b4146f4266c2877ba877cc57806e7
sorakim <sora6418@gmail.com>
2024-06-20 20:30:06 +0900

    Fix: 리뷰 관련 로직 변경

--- src/components/SideBar/SideBarReviews/SideBarReviews.jsx
+++ src/components/SideBar/SideBarReviews/SideBarReviews.jsx
@@ -2,8 +2,16 @@ import Modal from "../../Modal/Modal.jsx";
 import useModal from "../../../hooks/useModal.js";
 import { useEffect, useState } from "react";
 import { getReview } from "../../../lib/api/review.js";
-import styled from "styled-components";
-import { ReviewContainer, ReviewItem, ReviewItemList } from "./SideBarReviews.styled.jsx";
+import {
+  ReviewButton,
+  ReviewContainer,
+  ReviewImageWrapper,
+  ReviewItem,
+  ReviewItemContent,
+  ReviewItemList, ReviewNickName,
+} from "./SideBarReviews.styled.jsx";
+import { IconEdit } from "../../Icon/Icons/IconEdit.jsx";
+import Icon from "../../Icon/Icon.jsx";
 
 const SideBarReviews = () => {
   const { isOpen, openModal, closeModal, modalContent } = useModal();
@@ -22,19 +30,20 @@ const SideBarReviews = () => {
 
   return (
     <ReviewContainer>
-      <button onClick={() => openModal(<div>리뷰탭</div>)}>
-        Open Modal
-      </button>
       <Modal isOpen={isOpen} closeModal={closeModal}>
         {modalContent}
       </Modal>
+      <ReviewButton onClick={() => openModal()}>
+        리뷰쓰기
+      </ReviewButton>
       <ReviewItemList>
         {reviews.map(review => (
           <ReviewItem key={review.id}>
-            <div style={{width: '100%', height: '300px'}}>
+            <ReviewNickName>작성자 : {review.users.nickname}</ReviewNickName>
+            <ReviewImageWrapper>
               <img src={review.image} alt="review-img" />
-            </div>
-            <p>{review.content}</p>
+            </ReviewImageWrapper>
+            <ReviewItemContent>{review.content}</ReviewItemContent>
           </ReviewItem>
         ))}
       </ReviewItemList>
