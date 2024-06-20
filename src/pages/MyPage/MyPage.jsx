a47e300104072bae45b820865a5ff78a5a66aab2
Leeseayoung <tnfusrltk12@naver.com>
2024-06-20 23:27:01 +0900

    Add: styled 분리

--- src/pages/MyPage/MyPage.jsx
+++ src/pages/MyPage/MyPage.jsx
@@ -2,81 +2,20 @@ import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import Header from "../../components/Header/Header";
 import supabase from "../../supabaseClient";
-import MyPageModal from "./MyPageModal";
+import MyPageModal from "./MyPageModal/MyPageModal";
 import useModal from "../../hooks/useModal.js";
-
-const PageContainer = styled.div`
-  padding: 20px;
-`;
-
-const Section = styled.div`
-  margin-bottom: 20px;
-`;
-
-const Image = styled.img`
-  width: 150px;
-  height: auto;
-  margin-right: 10px;
-`;
-
-const ProfileContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
-
-const ProfileInfo = styled.div`
-  margin-top: 10px;
-`;
-
-const EditButton = styled.button`
-  background-color: var(--color-gray-normal);
-  color: white;
-  border: none;
-  padding: 5px 10px;
-  border-radius: 5px;
-  cursor: pointer;
-  margin-left: 15px;
-`;
-
-const DeleteButton = styled.button`
-  background-color: #dc3545;
-  color: white;
-  border: none;
-  padding: 5px 10px;
-  border-radius: 5px;
-  cursor: pointer;
-  margin-left: 10px;
-`;
-
-const ProfileLabel = styled.span`
-  font-weight: bold;
-  margin-right: 5px;
-`;
-
-const ReviewsContainer = styled.div`
-  margin-top: 20px;
-  border-top: 1px solid #ccc;
-  padding-top: 20px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
-
-const ReviewItem = styled.div`
-  background-color: #f9f9f9;
-  padding: 10px;
-  border-radius: 5px;
-  border: 1px solid #ddd;
-  margin-bottom: 15px;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-  width: 100%;
-  max-width: 700px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-top: 7px;
-`;
+import {
+  PageContainer,
+  Section,
+  Image,
+  ProfileContainer,
+  ProfileInfo,
+  EditButton,
+  DeleteButton,
+  ProfileLabel,
+  ReviewsContainer,
+  ReviewItem,
+} from "./MyPage.styled";
 
 const Mypage = () => {
   const [isEditingNickname, setIsEditingNickname] = useState(false);
@@ -175,67 +114,68 @@ const Mypage = () => {
   };
 
   return (
-    <PageContainer>
+    <div>
       <Header />
+      <PageContainer>
+        <Section>
+          <ProfileContainer>
+            <div>
+              <Image
+                src={profileImage ? profileImage : "이미지를 추가해주세요!"}
+                alt={profileImage ? "프로필 이미지" : "이미지를 추가해주세요!"}
+              />
+              <EditButton onClick={() => document.getElementById("fileInput").click()}>이미지 수정</EditButton>
+              <input
+                id="fileInput"
+                type="file"
+                accept="image/*"
+                style={{ display: "none" }}
+                onChange={(e) => handleImageUpload(e.target.files[0])}
+              />
+            </div>
+            <ProfileInfo>
+              <div>
+                <ProfileLabel>닉네임: </ProfileLabel>
+                {isEditingNickname ? (
+                  <input type="text" value={nickname} onChange={handleNicknameChange} />
+                ) : (
+                  <span>{userData?.nickname || "Loading..."}</span>
+                )}
+                <EditButton onClick={isEditingNickname ? handleNicknameSave : handleNicknameEdit}>
+                  {isEditingNickname ? "저장" : "수정"}
+                </EditButton>
+              </div>
+              <div>
+                <ProfileLabel>이메일:</ProfileLabel> <span>{userData?.email || "Loading..."}</span>
+              </div>
+            </ProfileInfo>
+          </ProfileContainer>
+        </Section>
+        <ReviewsContainer>
+          <div>내가 쓴 글</div>
+          {userReviews.map((review) => (
+            <ReviewItem key={review.id}>
+              <div>{review.content}</div>
+              <div>
+                <EditButton
+                  onClick={() => {
+                    setUserReviewId(review.id);
+                    openModal(<div>리뷰탭</div>);
+                  }}
+                >
+                  수정
+                </EditButton>
 
-      <Section>
-        <ProfileContainer>
-          <div>
-            <Image
-              src={profileImage ? profileImage : "이미지를 추가해주세요!"}
-              alt={profileImage ? "프로필 이미지" : "이미지를 추가해주세요!"}
-            />
-            <EditButton onClick={() => document.getElementById("fileInput").click()}>이미지 수정</EditButton>
-            <input
-              id="fileInput"
-              type="file"
-              accept="image/*"
-              style={{ display: "none" }}
-              onChange={(e) => handleImageUpload(e.target.files[0])}
-            />
-          </div>
-          <ProfileInfo>
-            <div>
-              <ProfileLabel>닉네임: </ProfileLabel>
-              {isEditingNickname ? (
-                <input type="text" value={nickname} onChange={handleNicknameChange} />
-              ) : (
-                <span>{userData?.nickname || "Loading..."}</span>
-              )}
-              <EditButton onClick={isEditingNickname ? handleNicknameSave : handleNicknameEdit}>
-                {isEditingNickname ? "저장" : "수정"}
-              </EditButton>
-            </div>
-            <div>
-              <ProfileLabel>이메일:</ProfileLabel> <span>{userData?.email || "Loading..."}</span>
-            </div>
-          </ProfileInfo>
-        </ProfileContainer>
-      </Section>
-      <ReviewsContainer>
-        <div>내가 쓴 글</div>
-        {userReviews.map((review) => (
-          <ReviewItem key={review.id}>
-            <div>{review.content}</div>
-            <div>
-              <EditButton
-                onClick={() => {
-                  setUserReviewId(review.id);
-                  openModal(<div>리뷰탭</div>);
-                }}
-              >
-                수정
-              </EditButton>
-
-              <DeleteButton onClick={() => handleReviewDelete(review.id)}>삭제</DeleteButton>
-            </div>
-          </ReviewItem>
-        ))}
-        <MyPageModal isOpen={isOpen} closeModal={closeModal} reviewId={userReviewId}>
-          {modalContent}
-        </MyPageModal>
-      </ReviewsContainer>
-    </PageContainer>
+                <DeleteButton onClick={() => handleReviewDelete(review.id)}>삭제</DeleteButton>
+              </div>
+            </ReviewItem>
+          ))}
+          <MyPageModal isOpen={isOpen} closeModal={closeModal} reviewId={userReviewId}>
+            {modalContent}
+          </MyPageModal>
+        </ReviewsContainer>
+      </PageContainer>
+    </div>
   );
 };
 
